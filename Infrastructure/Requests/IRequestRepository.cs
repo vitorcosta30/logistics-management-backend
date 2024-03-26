4f98ff061fca8852a6392cf92239d8ed0377f9c1
1201391 <1201391@isep.ipp.pt>
2024-03-26 11:44:23 +0000

    Created request repository

---
+++ Infrastructure/Requests/IRequestRepository.cs
@@ -0,0 +1,12 @@
+using logistics_management_backend.Domain.Goods;
+using logistics_management_backend.Domain.Requests;
+using logistics_management_backend.Domain.Shared;
+
+namespace logistics_management_backend.Infrastructure.Requests;
+
+public interface IRequestRepository : IRepository<Request>
+{
+    public Task<List<Request>> getAllToBeProcessedAsync();
+    public Task<List<RequestItemList>> getProductsInRequestAsync(long Id);
+
+}
--- Infrastructure/Requests/RequestRepository.cs
+++ Infrastructure/Requests/RequestRepository.cs
@@ -1,6 +1,27 @@
+using logistics_management_backend.Domain.Goods;
+using logistics_management_backend.Domain.Requests;
+using logistics_management_backend.Infrastructure.Shared;
+using Microsoft.EntityFrameworkCore;
+
 namespace logistics_management_backend.Infrastructure.Requests;
 
-public class RequestRepository
+public class RequestRepository : BaseRepository<Request>, IRequestRepository
 {
-    
+    private readonly DbSet<Request> _objs;
+
+    public RequestRepository(DbContextLM context) : base(context.Requests)
+    {
+        this._objs = context.Requests;
+    }
+
+    public async Task<List<Request>> getAllToBeProcessedAsync()
+    {
+        return await this._objs.Where(x => x.status.isToBeProcessed()).ToListAsync();
+
+    }
+
+    public async Task<List<RequestItemList>> getProductsInRequestAsync(long Id)
+    {
+        return await this._objs.Where(x => x.Id == Id).Select(x =>  x.listOfItems ).ToListAsync();
+    }
 }
