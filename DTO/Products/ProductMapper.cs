94864333d760a885b887049ef709468bf7bd13ad
1201391 <1201391@isep.ipp.pt>
2024-03-26 20:44:56 +0000

    Update product mapper

--- DTO/Products/ProductMapper.cs
+++ DTO/Products/ProductMapper.cs
@@ -8,4 +8,9 @@ public class ProductMapper
     {
         return new ProductDTO(product.Id, product.description, product.position.posX, product.position.posY);
     }
+
+    public static Product toDomain(ProductDTO dto)
+    {
+        return new Product(dto.description,dto.xPos,dto.yPos);
+    }
 }
