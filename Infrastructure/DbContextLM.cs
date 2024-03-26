8cd91223b1a692581c5106a6999c0dec965feaa7
1201391 <1201391@isep.ipp.pt>
2024-03-26 11:44:43 +0000

    Updated db context

--- Infrastructure/DbContextLM.cs
+++ Infrastructure/DbContextLM.cs
@@ -1,11 +1,14 @@
 using logistics_management_backend.Domain.Goods;
+using logistics_management_backend.Domain.Requests;
 using logistics_management_backend.Infrastructure.Products;
+using logistics_management_backend.Infrastructure.Requests;
 using Microsoft.EntityFrameworkCore;
 
 namespace logistics_management_backend.Infrastructure
 {
     public class DbContextLM : DbContext{
         public DbSet<Product> Products {get; set;} 
+        public DbSet<Request> Requests {get; set;} 
 
         
         public DbContextLM(DbContextOptions options) : base(options)
@@ -15,6 +18,12 @@ namespace logistics_management_backend.Infrastructure
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new ProductEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductPositionEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new RequestEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new RequestHistoryEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new RequestHistoryItemEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new RequestListEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new RequestListItemEntityTypeConfiguration());
 
 
 
