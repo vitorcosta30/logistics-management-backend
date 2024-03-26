9c73730a057cef8a2cd6937b02d4d00a74672384
1201391 <1201391@isep.ipp.pt>
2024-03-26 11:44:07 +0000

    Created product repository

---
+++ Infrastructure/Products/IProductRepository.cs
@@ -0,0 +1,9 @@
+using logistics_management_backend.Domain.Goods;
+using logistics_management_backend.Domain.Shared;
+
+namespace logistics_management_backend.Infrastructure.Products;
+
+public interface IProductRepository : IRepository<Product>
+{
+    
+}
--- Infrastructure/Products/ProductRepository.cs
+++ Infrastructure/Products/ProductRepository.cs
@@ -1,6 +1,15 @@
+using logistics_management_backend.Domain.Goods;
+using logistics_management_backend.Infrastructure.Shared;
+using Microsoft.EntityFrameworkCore;
+
 namespace logistics_management_backend.Infrastructure.Products;
 
-public class ProductRepository
+public class ProductRepository : BaseRepository<Product>, IProductRepository
 {
-    
+    private readonly DbSet<Product> _objs;
+    public ProductRepository(DbContextLM context) : base(context.Products)
+    {
+        this._objs = context.Products;
+
+    }
 }
